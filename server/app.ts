652430f56d17c6e81a9057357c40639105b8aae2
Osama Khan <khanosama783@gmail.com>
2024-01-11 17:04:30 +0530

    first commit

---
+++ server/app.ts
@@ -0,0 +1,35 @@
+import express, { NextFunction, Request, Response } from "express";
+export const app = express();
+import cors from "cors";
+import cookieParser from "cookie-parser";
+require("dotenv").config();
+
+//body parser
+app.use(express.json({ limit: "50mb" }));
+
+//cookie parsr
+
+app.use(cookieParser());
+
+//cors
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
+
+// testing api
+
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    success: true,
+    message: " Api is fine",
+  });
+});
+
+//unknown route
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not found`) as any;
+  err.statusCode = 404;
+  next(err);
+});
