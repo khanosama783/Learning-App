1cdd80a438b2313d1b2f4e12e10ebcebb48c77d2
Osama Khan <khanosama783@gmail.com>
2024-01-12 21:08:59 +0530

    Error Handler added

--- server/app.ts
+++ server/app.ts
@@ -3,6 +3,7 @@ export const app = express();
 import cors from "cors";
 import cookieParser from "cookie-parser";
 require("dotenv").config();
+import { ErrorMiddleware } from "./middleware/error";
 
 //body parser
 app.use(express.json({ limit: "50mb" }));
@@ -33,3 +34,5 @@ app.all("*", (req: Request, res: Response, next: NextFunction) => {
   err.statusCode = 404;
   next(err);
 });
+
+app.use(ErrorMiddleware);
