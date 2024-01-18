e1858dc06b1f16c61a751a06d3c66f83fdb98c19
Osama Khan <khanosama783@gmail.com>
2024-01-18 17:36:56 +0530

    fix password error, add social authentication

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import {
   activateUser,
+  getUserInfo,
   loginUser,
   logoutUser,
   resgistrationUser,
+  socialAuth,
+  updateAccessToken,
 } from "../controllers/user.controllers";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 const userRouter = express.Router();
@@ -12,5 +15,8 @@ userRouter.post("/registration", resgistrationUser);
 userRouter.post("/activate-user", activateUser);
 userRouter.post("/login", loginUser);
 userRouter.get("/logout", isAuthenticated, authorizeRoles("admin"), logoutUser);
+userRouter.get("/refresh", updateAccessToken);
+userRouter.get("/me", isAuthenticated, getUserInfo);
+userRouter.post("/social-auth", socialAuth);
 
 export default userRouter;
--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -11,37 +11,45 @@ interface ITokenOptions {
   sameSite: "lax" | "strict" | "none" | undefined;
   secure?: boolean;
 }
+
+//parse enviroment varable to integrates with fallback
+export const accessTokenExpire = parseInt(
+  process.env.ACCESS_TOKEN_EXPIRE || "300",
+  10
+);
+export const refreshTokenExpire = parseInt(
+  process.env.REFRESH_TOKEN_EXPIRE || "1200",
+  10
+);
+
+export const accessTokenOptions: ITokenOptions = {
+  expires: new Date(Date.now() + accessTokenExpire * 60 * 60 * 1000),
+  maxAge: accessTokenExpire * 60 * 60 * 1000,
+  httpOnly: true,
+  sameSite: "lax",
+};
+
+export const refreshTokenOptions: ITokenOptions = {
+  expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000),
+  maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
+  httpOnly: true,
+  sameSite: "lax",
+};
+
 export const sendToken = (user: IUser, statusCode: number, res: Response) => {
   const accessToken = user.SignAccessToken();
   const refreshToken = user.SignRefreshToken();
   // send session to redis
   redis.set(user._id, JSON.stringify(user) as any);
-  //parse enviroment varable to integrates with fallback
-  const accessTokenExpire = parseInt(
-    process.env.ACCESS_TOKEN_EXPIRE || "300",
-    10
-  );
-  const refreshTokenExpire = parseInt(
-    process.env.REFRESH_TOKEN_EXPIRE || "1200",
-    10
-  );
-  const accessTokenOptions: ITokenOptions = {
-    expires: new Date(Date.now() + accessTokenExpire * 1000),
-    maxAge: accessTokenExpire * 1000,
-    httpOnly: true,
-    sameSite: "lax",
-  };
-  const refreshTokenOptions: ITokenOptions = {
-    expires: new Date(Date.now() + refreshTokenExpire * 1000),
-    maxAge: refreshTokenExpire * 1000,
-    httpOnly: true,
-    sameSite: "lax",
-  };
+
   if (process.env.NODE_ENV === "production") {
     accessTokenOptions.secure = true;
   }
+
   res.cookie("access_token", accessToken, accessTokenOptions);
+
   res.cookie("refresh_token", refreshToken, refreshTokenOptions);
+
   res.status(statusCode).json({
     success: true,
     user,
