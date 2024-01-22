727560bc45215d7ba993882e60fc94d97dcdcfc5
Osama Khan <khanosama783@gmail.com>
2024-01-22 17:48:28 +0530

    create question, add reply in question

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -5,6 +5,8 @@ import {
   getSingleCourse,
   getAllCourses,
   getCourseByUser,
+  addQuestion,
+  addAnswer,
 } from "../controllers/course.controller";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 //const courseRouter = express.Router();
@@ -30,5 +32,9 @@ courseRouter.get("/get-course/:id", getSingleCourse);
 courseRouter.get("/get-courses", getAllCourses);
 
 courseRouter.get("/get-course-content/:id", isAuthenticated, getCourseByUser);
+
+courseRouter.put("/add-question", isAuthenticated, addQuestion);
+
+courseRouter.put("/add-answer", isAuthenticated, addAnswer);
 
 export default courseRouter;
