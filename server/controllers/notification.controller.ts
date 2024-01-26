79374a5dba6d6490837ce353550b131744c19c31
Osama Khan <khanosama783@gmail.com>
2024-01-26 16:17:18 +0530

    add cron, notification delete for admin

--- server/controllers/notification.controller.ts
+++ server/controllers/notification.controller.ts
@@ -2,6 +2,7 @@ import NotifactionModel from "../models/notificationModel";
 import { NextFunction, Request, Response } from "express";
 import { CatchAsyncError } from "../middleware/catchAsyncError";
 import ErrorHandler from "../utils/ErrorHandler";
+import cron from "node-cron";
 
 //get all notification for admin
 export const getNotifications = CatchAsyncError(
@@ -51,3 +52,13 @@ export const updateNotification = CatchAsyncError(
     }
   }
 );
+
+//delete notification for admin
+cron.schedule("0 0 0  * * *", async () => {
+  const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 100);
+  await NotifactionModel.deleteMany({
+    status: "read",
+    createdAt: { $lt: thirtyDaysAgo },
+  });
+  console.log("Deleted read notification");
+});
