0b89afcc23fc889e552927524da44b8af6849d95
Osama Khan <khanosama783@gmail.com>
2024-06-24 18:34:15 +0530

    fix minor typo and create admin sidebar structure

---
+++ client/app/admin/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React from "react";
+import Heading from "../utils/Heading";
+import AdminSidebar from "../components/Admin/sidebar/AdminSidebar";
+import AdminProtected from "../hooks/adminProtected";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <div>
+      <AdminProtected>
+        <Heading
+          title="Admin Deshboard"
+          description="This is a platform for students to learn and get help from teachers"
+          keywords="Data Science, Devops, MLops, Data engineer, Machine Learning"
+        />
+        <div className="flex h-[200vh]">
+          <div className="1500px:w-[16%] w-1/5">
+            <AdminSidebar />
+          </div>
+          <div className="w-[85%]"></div>
+        </div>
+      </AdminProtected>
+    </div>
+  );
+};
+
+export default page;
--- client/app/components/Route/Hero.tsx
+++ client/app/components/Route/Hero.tsx
@@ -17,7 +17,9 @@ const Hero: FC<Props> = (props) => {
           />
         </div>
         <div className="1000px:w-[60%] flex flex-col items-center 1000px:mt-[0px] text-center 1000px:text-left mt-[150px]">
-          <h2>Impove Your Online Learning Experience Better Instantly</h2>
+          <h2 className="dark:text-white text-[#000000c7] text-[30px] px-3 w-full 1000px:text-[70px] font-[600] font-Josefin py-2 1000px:leading-[75px] 1500px:">
+            Impove Your Online Learning Experience Better Instantly
+            </h2>
           <br />
           <p className="dark:text-[#edfff4] text-[#000000ac] font-Josefin font-[600] text-[18px] 1500px:!w-[55%] 1100px:!w-[78%]">
             We have 40k+ Online course & 500k+ Online registered Student. Find
